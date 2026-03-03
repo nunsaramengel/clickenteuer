18e22ba6abd7ab73310923afd93e379fb8515c3e
Nvssvvxx <45807893+nunsaramengel@users.noreply.github.com>
2026-03-03 23:18:35 +0100

    Update image URL for 'rabe' text
    rabe img

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const STORY = {
   },
   rabe: {
     text: "Der Rabe lässt einen silbernen Schlüssel fallen und fliegt davon.",
-    image: "https://unsplash.com/photos/a-black-bird-sitting-on-top-of-a-tree-branch-tCRflqaRxBo?w=800",
+    image: "https://images.unsplash.com/photos/a-black-bird-sitting-on-top-of-a-roof-d-vBDK5rqR0?w=800",
     options: [
       { text: "Den Schlüssel nehmen", next: "ende_gut" }
     ]
